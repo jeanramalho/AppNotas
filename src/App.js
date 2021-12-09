4cef2e3cae60cd2b83681ede5a21dfee6922528b
Jean <jeanramalho.dev@gmail.com>
2021-12-09 17:19:44 -0300

    Inicia o Projeto de uma aplicação de notas simples com React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ListaDeNotas from './components/ListaDeNotas'
+
+
+function App() {
+  return (
+    <section>
+  <form>
+    <input type="text" placeholder="Título"/>
+    <textarea placeholder="Escreva sua nota..."></textarea>
+    <button>Criar Nota</button>
+  </form>
+  <ListaDeNotas/>
+  </section>
+  )
+}
+
+export default App;

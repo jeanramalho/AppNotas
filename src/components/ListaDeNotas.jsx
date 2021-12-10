f34e4727a0ced6968d51b11d446ba6f3f111fb22
Jean <jeanramalho.dev@gmail.com>
2021-12-10 13:43:56 -0300

    Começa a implementar estilo css nos components

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react"
-import CardNota from "./CardNota"
+import CardNota from "./CardNota/CardNota"
 
 class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
-        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria) => {
-          return (<li>
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
+          return (<li key={index}>
             <div>{categoria}</div>
             <CardNota />
           </li>
